2cf5455d033b56440064cd38a40deaa0d191bcf3
Tesla Junior <teslajunior0552@gmail.com>
2025-10-10 17:40:59 +0000

    Update ProjectsArray.jsx

--- ProjectsArray.jsx
+++ ProjectsArray.jsx
@@ -2,7 +2,14 @@ import tenziesImg from "./tenzies.png"
 import travelJournalImg from "./travel-journal.png"
 import memeGeneratorImg from  "./meme-generator.png"
 import shopcartImg from "./shopcart.jpg"
+import lifeEndGame from "./endgame.png"
 export const projects = [
+    {
+        img: lifeEndGame,
+        name: Life EndGame,
+        description: "Users are giving the chance to save themselves from a lonely world by guessing a hidden word, any wrong guess pay's a penalty.",
+        link: "https://life-endgame-jvuv.vercel.app/"
+    },
     {
         img: tenziesImg,
         name: "Tenzies",
@@ -31,4 +38,5 @@ export const projects = [
         description: "shopcart is an app that assist in making shopping list, enter an item click add to cart and it is displayed in the cart. When you are done buying any item, double click on it to delete from cart.",
         link: "https://mummy-silk.vercel.app/"
     }
+
 ]
